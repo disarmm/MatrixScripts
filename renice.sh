3a1314284409f656ab8ef114e00fcb931b5e2944
disarmm <leonardsjunk@gmail.com>
2019-11-29 01:46:39 +0000

    removed echo

--- renice.sh
+++ renice.sh
@@ -4,7 +4,6 @@ for c in $(docker ps | grep nodeConfig | awk {'print $1'}) ; do
 	miningStatus=$(docker exec ${c} /bin/bash -c "/matrix/gman attach /matrix/chaindata/gman.ipc -exec man.mining")
 	entrypointPID=$(docker inspect --format '{{.State.Pid}}' ${c})
 	gmanPID=$(pstree -p ${entrypointPID} | cut -d "(" -f 3 | cut -d ")" -f 1)
-	echo ${gmanPID}
 	if [ miningStatus == "true" ] ; then
 		renice -n -20 -p ${gmanPID}
 	else
