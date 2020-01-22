5ffc5ca9c5910aaa56e4d04a077246d394253e0a
disarmm <49657913+disarmm@users.noreply.github.com>
2020-01-22 17:53:37 -0600

    Create maintAdd.sh

---
+++ maintAdd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -P /usr/local/bin/ https://raw.githubusercontent.com/disarmm/MatrixScripts/master/nodeMaint.sh
+mv /usr/local/bin/nodeMaint.sh /usr/local/bin/nodeMaint
+chmod a+x /usr/local/bin/nodeMaint
